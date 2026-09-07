dc42f3369a5d2201d53330599da6e88be4e66a51
Eccomi OnLine <ciaoeccomionline@gmail.com>
2026-09-07 08:48:55 +0200

    EVS V20 register smart audio mix task

--- cpu_workflow/bootstrap.py
+++ cpu_workflow/bootstrap.py
@@ -1,7 +1,9 @@
 import main
 from commercial_task_v4 import register_commercial
+from smart_audio_task import register_smart_audio
 
 register_commercial(main.app)
+register_smart_audio(main.app)
 
 if __name__ == "__main__":
     main.app.start()
