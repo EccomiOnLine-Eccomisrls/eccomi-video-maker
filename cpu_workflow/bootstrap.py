3e5e273c7abf22ab111dcb3bf15ea715c2ffb427
Eccomi OnLine <ciaoeccomionline@gmail.com>
2026-09-08 23:02:15 +0200

    Register EVS Mascotte AI CPU finalizer task

--- cpu_workflow/bootstrap.py
+++ cpu_workflow/bootstrap.py
@@ -1,9 +1,11 @@
 import main
 from commercial_task_v4 import register_commercial
 from smart_audio_task import register_smart_audio
+from mascot_task import register_mascot
 
 register_commercial(main.app)
 register_smart_audio(main.app)
+register_mascot(main.app)
 
 if __name__ == "__main__":
     main.app.start()
