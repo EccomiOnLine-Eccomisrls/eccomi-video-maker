376a433be364a4097c3fd161b3244c9190436f8e
Eccomi OnLine <ciaoeccomionline@gmail.com>
2026-09-10 15:19:26 +0200

    EVS: register surgical repair task in Render workflow

--- cpu_workflow/bootstrap.py
+++ cpu_workflow/bootstrap.py
@@ -2,10 +2,12 @@ import main
 from commercial_task_v4 import register_commercial
 from smart_audio_task import register_smart_audio
 from mascot_task import register_mascot
+from surgical_task import register_surgical
 
 register_commercial(main.app)
 register_smart_audio(main.app)
 register_mascot(main.app)
+register_surgical(main.app)
 
 if __name__ == "__main__":
     main.app.start()
